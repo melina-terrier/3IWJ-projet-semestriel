29ec348fc578df8c8d0989b9e5e2267bf0323ddf
azer <azermami8@gmail.com>
2024-05-16 10:34:37 +0200

    ajout des controllers et mise à jour du code

---
+++ www/Controllers/Projet.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Controller;
+class Projet
+{
+
+    public function delete(): void
+    {
+
+    }
+    public function show(): void
+    {
+        echo "Affichage d'un projet";
+    }
+    public function edit(): void
+    {
+    
+    }
+    public function add(): void
+    {
+
+    }
+   
+
+}
--- www/Controllers/Security.php
+++ www/Controllers/Security.php
@@ -1,6 +1,10 @@
 <?php
 namespace App\Controller;
+use App\Core\Form;
 use App\Core\Security as Auth;
+use App\Core\View;
+use App\Models\User;
+
 class Security{
 
 
@@ -8,18 +12,44 @@ class Security{
     {
         //Je vérifie que l'utilisateur n'est pas connecté sinon j'affiche un message
 
+        /*
         $security = new Auth();
         if($security->isLogged()){
             echo "Vous êtes déjà connecté";
         }else{
             echo "Se connecter";
         }
+        */
+
+        $form = new Form("Login");
+        if( $form->isSubmitted() && $form->isValid() )
+        {
+
+        }
+        $view = new View("Security/login");
+        $view->assign("form", $form->build());
+        $view->render();
 
 
     }
     public function register(): void
     {
-        echo "S'inscrire";
+
+        $form = new Form("Register");
+
+        if( $form->isSubmitted() && $form->isValid() )
+        {
+            $user = new User();
+            $user->setFirstname($_POST["firstname"]);
+            $user->setLastname($_POST["lastname"]);
+            $user->setEmail($_POST["email"]);
+            $user->setPassword($_POST["password"]);
+            $user->save();
+        }
+
+        $view = new View("Security/register");
+        $view->assign("form", $form->build());
+        $view->render();
     }
     public function logout(): void
     {
