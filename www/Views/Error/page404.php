2f949f35f26680b8b80dddc62700b1a3744a568e
“Mélina <mterrier4@myges.fr>
2024-06-16 13:37:34 +0200

    Ajout de vite et de SASS

--- www/Views/Error/page404.php
+++
@@ -1,5 +0,0 @@
-<section>
-    <h1>Page introuvable</h1>
-    <p>La page n'existe pas. Vous pouvez vérifier le lien ou faire une recherche ci-dessus pour trouver ce que vous cherchez.</p>
-    <a href="/" class="Button Primary">Retour à la page d'accueil</a>
-</section>
--- www/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Configuration de la base de données
-define('DB_HOST', 'postgres');
-define('DB_PORT', '5432');
-define('DB_NAME', 'msnu');
-define('DB_USER', 'esgi');
-define('DB_PASSWORD', 'esgipwd');
-define('TABLE_PREFIX', 'msnu');
