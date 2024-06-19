707f1d2439139c5ac2422823fa52b74b0c6aed46
azer <azermami8@gmail.com>
2024-06-19 18:22:08 +0200

    debut d integration

--- www/Views/Main/dashboard.php
+++ www/Views/Main/dashboard.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 <main>
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"> </script>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
         google.charts.load('current', {'packages':['corechart']});
         google.charts.setOnLoadCallback(drawChart);
